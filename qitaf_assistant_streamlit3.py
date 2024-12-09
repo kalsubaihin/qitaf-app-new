18f1ea48d4b4e4c44d8fcc027cdac6bcc184acf6
kalsubaihin <khaled@hawa.co>
2024-12-09 15:10:57 +0300

    4o-mini

--- qitaf_assistant_streamlit3.py
+++ qitaf_assistant_streamlit3.py
@@ -155,7 +155,7 @@ client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 # Set a default model
 if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-4"
+    st.session_state["openai_model"] = "gpt-4o-mini"
 
 # Initialize chat history with system message
 if "messages" not in st.session_state:
